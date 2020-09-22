62a16a7130b6e357dbdf409011fd87310edea64b
Falanteris <rayhanrts@gmail.com>
2020-09-22 19:44:23 +0700

    Fixed Exported Projects

--- exported_forensic_project.js
+++ exported_forensic_project.js
@@ -1,23 +1,26 @@
+const { exit } = require("process");
+
 var ls = require("./file_listener").listener;
 
-var new_listener = new ls("C:\\Users\\RAYHAN","RAYHANlog.txt");
+var new_listener = new ls(process.argv[2],process.argv[3]);
 
-console.log(new_listener);
+//console.log(new_listener);
 
 var attempt_prm = new Promise(function(resolve,reject){
 	try{
 		new_listener.listen();
 		resolve("Attempt Success");
 	}
-	catch{
-
+	catch(error){
+		console.log(error)
 		reject(new Error("Listen Attempt Error"));	
 	}
 }).then((result) =>{
 	console.log(result);
 
 }).catch((err) => {
-	throw err;
+	console.log(err);
+	exit();
 }).then(() => {
 	console.log(" End Of Attempt..");
 });
