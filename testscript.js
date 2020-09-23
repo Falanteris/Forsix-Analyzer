50a1006647c8ace40e9ea5b88accbb14011eee21
Falanteris <rayhanrts@gmail.com>
2020-09-23 16:51:21 +0700

    Fixed Package.json

--- testscript.js
+++ testscript.js
@@ -55,7 +55,7 @@ try {
             case "dir":
                 spawnSync("mkdir",["-p",global_conf.testsArgs[index].name])
                 let d = spawnSync("tail",["activity.log"])
-                console.log(d)
+                console.log(d.stderr.toString())
                 console.log("Currently tested : " + global_conf.testsArgs[index].name)
                 let dat = d.stdout.toString().split("\n")
                 notDeepStrictEqual(dat.remove('')[dat.length-1].search(global_conf.testsArgs[index].name),-1)
@@ -63,7 +63,7 @@ try {
             case "file":
                 spawnSync("touch",[global_conf.testsArgs[index].name]);
                 let d2= spawnSync("tail",["activity.log"])
-                console.log(d2)
+                console.log(d2.stderr.toString())
                 console.log("Currently tested : " + global_conf.testsArgs[index].name)
                 let dat2 = d2.stdout.toString().split("\n")
                 notDeepStrictEqual(dat2.remove('')[dat2.length-1].search(global_conf.testsArgs[index].name),-1)
